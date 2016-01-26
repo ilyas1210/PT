b10aa4b76aa79b0a5c99fcd7aa4e4f066b58b766
ilyas <ker.ilyas@mail.ru>
2016-01-26 12:36:01 +0600

    test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string s; //числа вводим
+            s = Console.ReadLine(); //chitaet chisla
+            string[] stringNumbers = s.Split();// delim ih na raznie
+            foreach (string ss in stringNumbers) // probegaemsya po stringu
+            {
+                int a= Int32.Parse(ss); //perevodim iz stringa v integer form
+                bool ok = true; 
+                for (int i = 2; i<a; i++) //uslovie prostogo chisla
+                {
+                    if (a % i == 0) ok = false; //esli delitsya na drugie chisla to false
+                }
+                if (ok == true) // dlya vseh ostalnih znachenie istionoe 
+                    Console.WriteLine(a);
+            }
+            Console.ReadKey();
         }
+        
+
+
     }
 }
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo()
+            int cnt = Search(d);
+            Console.WriteLine(cnt);
+            Console.ReadKey();
+        }
+    static int Search(DirectoryInfo d);
+    {
+        fileinfo[] Files = d.GetFiles();
+        int cnt_files = files.Length; 
+        return cnt_files;
+    }
+    }
+
+}
