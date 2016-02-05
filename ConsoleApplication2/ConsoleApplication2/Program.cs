0b01f8b7f9e9211703ea7ca7a91de750294557fa
ilyas <ker.ilyas@mail.ru>
2016-02-05 11:38:17 +0600

    added lab 2

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,27 +1,79 @@
 ﻿using System;
-using System.IO;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace lab2
 {
+
     class Program
     {
+
+        public bool isPrime(int a)
+        {
+
+            for (int i = 2; i * i <= a; i++)
+
+                if (a % i == 0) return false;
+
+            return true;
+
+        }
+
         static void Main(string[] args)
         {
-            DirectoryInfo d = new DirectoryInfo()
-            int cnt = Search(d);
-            Console.WriteLine(cnt);
+
+            string text = System.IO.File.ReadAllText(@"C:\Users\Ilyas K\Desktop\a.txt.txt");
+
+            string[] ss = text.Split();
+
+            Console.WriteLine(text);
+
             Console.ReadKey();
+
+            int[] numbers = new int[ss.Length];
+
+            for (int i = 0; i < ss.Length; i++)
+            {
+
+                numbers[i] = int.Parse(ss[i]);
+
+            }
+
+            int maxi = numbers.Max(), mini = numbers.Min();
+
+            int minPrime = numbers[0];
+
+            Program p = new Program();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (p.isPrime(numbers[i]))
+                {
+
+                    if (minPrime > numbers[i])
+                    {
+
+                        minPrime = numbers[i];
+
+                    }
+
+                }
+
+            }
+
+            string text2 = text + "nextline " + maxi.ToString() + " " + mini.ToString() + " " + minPrime.ToString();
+
+            System.IO.File.WriteAllText(@"C:\Users\Ilyas K\Desktop\a.txt.txt", text2);
+
         }
-    static int Search(DirectoryInfo d);
-    {
-        fileinfo[] Files = d.GetFiles();
-        int cnt_files = files.Length; 
-        return cnt_files;
-    }
+        
     }
 
 }
